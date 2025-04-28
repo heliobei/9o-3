b4853f4ced27bf62909048c82533a90d5924cf20
heliobei <suporte@beieducacaopedagogico.com>
2025-04-28 12:07:32 -0300

    Update MatVideo1.jsx

--- src/app/components/Matematica/MatVideo1.jsx
+++ src/app/components/Matematica/MatVideo1.jsx
@@ -11,7 +11,7 @@ function MatVideo1() {
           <div className="attention-box mb-8 mx-8 text-slate-800 text-center text-lg border-2 border-primary rounded-lg p-8">
               <h1 className="text-4xl font-semibold text-slate-700 mb-4">Atividades Extras</h1>
                 <p>
-                    Acesse as <a href="https://exerciciosextras6o.beieducacaopedagogico.com/" target="_blank" rel="noopener noreferrer" className="underline text-blue-800 hover:text-purple-800"> atividades extras</a>{' '}que vão além do conteúdo presente nos livros físicos. Aqui você poderá acessar e baixar PDFs com atividades para cada capítulo, dando um passo além no ensino de habilidades matemáticas.
+                    Acesse as <a href="https://exerciciosextras9o.beieducacaopedagogico.com/" target="_blank" rel="noopener noreferrer" className="underline text-blue-800 hover:text-purple-800"> atividades extras</a>{' '}que vão além do conteúdo presente nos livros físicos. Aqui você poderá acessar e baixar PDFs com atividades para cada capítulo, dando um passo além no ensino de habilidades matemáticas.
                 </p>
                 
               </div>
